e16f21b7f2d698aa5fce7ecbc77f4571a17eaf61
khaled romdhane <khaled@medgo.fr>
2020-10-08 10:00:30 +0200

    adding api call

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [msg, setMsg] = useState(
+    "Edit <code>src/App.js</code> and save to reload."
+  );
+  fetch("http://localhost:4000/api").then((data) => {
+    data.json().then((resBody) => {
+      setMsg(resBody.msg);
+    });
+  });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{msg}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
